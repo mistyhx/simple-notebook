ea3ac39eca1712065ee0df4709d6b22192ff00d0
xin.hu <xin.hu@xinhus-MacBook-Pro.local>
2020-06-22 23:33:58 -0400

    added localstorage

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import NoteEditor from "./components/NoteEditor";
 import "./App.css";
 
 function App() {
-  const [notes, setNotes] = useState("");
+  const [notes, setNotes] = useState(JSON.parse(window.localStorage.getItem("notes")));
+  useEffect(() => {
+    window.localStorage.setItem("notes", JSON.stringify({ key: "001", title: "Javascript", content: "My content" }));
+    console.log(window.localStorage);
+  }, [notes]);
   return (
     <div className="App">
       <div className="note">
-        <NoteList />
+        <NoteList notes={notes} />
         <NoteEditor />
       </div>
     </div>
