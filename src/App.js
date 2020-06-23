7ae453a8b5871a199e1b384d12ccab86bb455546
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-06-23 09:22:58 -0400

    added add new note button withstate change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Plus } from "react-feather";
 import NoteList from "./components/NoteList";
 import NoteEditor from "./components/NoteEditor";
 import "./App.css";
@@ -10,10 +11,11 @@ function App() {
   ];
   const [notes, setNotes] = useState(JSON.parse(window.localStorage.getItem("notes")));
   const newNote = () => {
-    // const temp = [...notes];
-    // const newNote = { createOn: new Date(), title: "New note", content: "new note" };
-    // temp.push(newNote);
-    // setNotes(temp);
+    const temp = notes;
+    const newNote = { createOn: new Date(), title: "New note", content: "new note" };
+    temp.push(newNote);
+    console.log(temp);
+    setNotes(temp);
   };
   useEffect(() => {
     window.localStorage.setItem("notes", JSON.stringify(tempNote));
@@ -25,7 +27,7 @@ function App() {
         <NoteList notes={notes} />
         <NoteEditor />
         <div className="button-add" onClick={() => newNote()}>
-          +
+          <Plus size={36} />
         </div>
       </div>
     </div>
