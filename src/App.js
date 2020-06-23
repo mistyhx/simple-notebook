4577713044040615f8bb0012e70f1aae5ba5cb81
xin.hu <xin.hu@xinhus-MacBook-Pro.local>
2020-06-23 08:18:59 -0400

    set up mock note data in local storage

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import NoteEditor from "./components/NoteEditor";
 import "./App.css";
 
 function App() {
+  const tempNote = [
+    { key: 1, title: "my note", content: "my note1" },
+    { key: 2, title: "my 2 note", content: "my note 2" },
+  ];
   const [notes, setNotes] = useState(JSON.parse(window.localStorage.getItem("notes")));
   const newNote = () => {
-    const temp = [...notes];
-    const newNote = { createOn: new Date(), title: "New note", content: "new note" };
-    temp.push(newNote);
-    setNotes(temp);
+    // const temp = [...notes];
+    // const newNote = { createOn: new Date(), title: "New note", content: "new note" };
+    // temp.push(newNote);
+    // setNotes(temp);
   };
   useEffect(() => {
-    window.localStorage.setItem("notes", JSON.stringify(notes));
+    window.localStorage.setItem("notes", JSON.stringify(tempNote));
     console.log(window.localStorage);
   }, [notes]);
   return (
